12bc471ddaf51bba949ec67680e6afbc97926244
dikokkali <dikokkaliaroglou@gmail.com>
2021-10-08 20:09:52 +0300

    20211008-commit: first commit

---
+++ DemoApi/DemoApi/Controllers/DemoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApi.Controllers
+{    
+    [ApiController]
+    [Route("Demo")]
+    public class DemoController
+    {
+        [HttpGet]
+        public String Get()
+        {
+            return "Hello World";
+        }
+
+    }
+}
