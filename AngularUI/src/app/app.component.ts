3ebf48a1376542b5d3bad4dd4bd554f0d5df6a4d
dikokkali <dikokkaliaroglou@gmail.com>
2021-10-08 20:43:07 +0300

    20211008-commit: Added Angular frontend with test API access

--- AngularUI/src/app/app.component.ts
+++ AngularUI/src/app/app.component.ts
@@ -1,4 +1,6 @@
+
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularUI';
+  title = 'DemoApp';
+  response = "No data loaded, yet";
+  constructor(private http: HttpClient) 
+  { 
+    this.http.get('http://localhost/demo', {responseType: 'text'}).subscribe((response: any) => {
+      console.log(response);
+	  this.response = response;		
+	});
+  }  
 }
